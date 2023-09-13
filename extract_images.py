4f59fe7c92f751c4ff22a278d68361efbb4b137e
Bruce Van Horn <brucevanhorn@me.com>
2023-09-12 22:55:48 -0500

    extracted images are in the order in which they appear in the text (this time it seems to work)

--- extract_images.py
+++ extract_images.py
@@ -1,34 +1,37 @@
 import os
 import sys
-from docx import Document
-
+import re
+import docx2txt
 
 def extract_images_from_docx(docx_file, output_folder):
     # Create the output folder if it doesn't exist
     os.makedirs(output_folder, exist_ok=True)
 
-    # Load the docx file
-    doc = Document(docx_file)
+    # Use docx2txt to convert the DOCX document to plain text
+    text = docx2txt.process(docx_file, output_folder)
+
+    # Define a regular expression to find image references in the plain text
+    image_pattern = re.compile(r'!\[.*?\]\((.*?)\)')
+
+    # Find all image references in the plain text
+    image_matches = image_pattern.findall(text)
 
     # Counter for the image index
     image_index = 0
 
-    # Iterate over all inline shapes in the document
-    for inline_shape in doc.inline_shapes:
-        if inline_shape.type == 3:  # InlineShapeType.PICTURE
-            # Get the image data
-            image_data = inline_shape._inline.graphic.graphicData.pic.blip.getbytes()
-
-            # Save the image to the output folder
-            image_filename = f"image_{image_index}.png"
-            image_path = os.path.join(output_folder, image_filename)
-            with open(image_path, "wb") as f:
-                f.write(image_data)
-
-            image_index += 1
+    for image_match in image_matches:
+        # Check if the image reference contains a valid image file extension (e.g., .png, .jpg)
+        if any(image_match.lower().endswith(ext) for ext in ['.png', '.jpg', '.jpeg', '.gif']):
+            # Copy the image file to the output folder
+            image_path = os.path.join(os.path.dirname(docx_file), image_match)
+            if os.path.exists(image_path):
+                image_filename = f"image_{image_index}.png"  # Rename the image
+                image_output_path = os.path.join(output_folder, image_filename)
+                with open(image_path, "rb") as source, open(image_output_path, "wb") as destination:
+                    destination.write(source.read())
+                image_index += 1
 
     print("Image extraction completed!")
-
 
 if __name__ == "__main__":
     # Check if the script is invoked with the correct number of arguments
